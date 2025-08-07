90b2e29ff113cfea942711b8eb1baa1174d56dca
lzh <lizhi916332@gmail.com>
2025-08-07 18:25:11 +0800

    面经基础版-路由

---
+++ l7/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <keep-alive :exclude="['XiangQing']">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="less">
+*, body, html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app,body,html {
+  min-height: 100vh;
+}
+</style>
