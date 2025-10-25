a7e5c5cef90d3e103aeb6d729b8d9e7add34f76f
lzh <lizhi916332@gmail.com>
2025-10-25 22:43:21 +0800

    + 创建二级路由页面导航 ++ 按需导入Tabbar，TabbarItem组件 ++ 在LayoutPage创建导航栏

--- mj-mobile/src/utils/vant-ui.js
+++ mj-mobile/src/utils/vant-ui.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'vant'
+// import { Button } from 'vant'
+import { Tabbar, TabbarItem } from 'vant'
 
 // vant-ui全部导入
 // import Vant from 'vant'
 // import 'vant/lib/index.css'
 // Vue.use(Vant)
 
-Vue.use(Button)
+// Vue.use(Button)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
--- mj-mobile/src/views/LayoutPage.vue
+++ mj-mobile/src/views/LayoutPage.vue
@@ -6,7 +6,14 @@ export default {
 
 <template>
 <div>
-  layout
+  <div>内容区域</div>
+  <!--  底部二级路由导航栏-->
+  <van-tabbar>
+    <van-tabbar-item icon="fire-o">面经</van-tabbar-item>
+    <van-tabbar-item icon="star-o">收藏</van-tabbar-item>
+    <van-tabbar-item icon="like-o">喜欢</van-tabbar-item>
+    <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+  </van-tabbar>
 </div>
 </template>
 
